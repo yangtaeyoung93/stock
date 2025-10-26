d75efc49048b2636d72192decf4b1fc62554547e
yangtaeyoung93 <ngj100@naver.com>
2025-10-27 00:47:18 +0900

    lettuce, Redisson 방식 동시성 문제 추가

---
+++ src/main/java/com/example/stock/repository/RedisLockRepository.java
@@ -0,0 +1,31 @@
+package com.example.stock.repository;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+public class RedisLockRepository {
+
+    private RedisTemplate<String,String> redisTemplate;
+
+    public RedisLockRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Boolean lock(long key) {
+        return redisTemplate
+                .opsForValue()
+                .setIfAbsent(generateKey(key), "lock", Duration.ofMillis(3000));
+    }
+
+
+    public Boolean unLock(Long key) {
+        return redisTemplate.delete(generateKey(key));
+    }
+
+    private String generateKey(Long key) {
+        return key.toString();
+    }
+}
